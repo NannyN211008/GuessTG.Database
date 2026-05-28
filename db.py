8281154fb3caed87528d2a0f879570940868f056
NannyN211008 <naren.niranjanarajah@education.nsw.gov.au>
2026-05-29 09:50:57 +1000

    Add login, logout, and session handling functionality

--- db.py
+++ db.py
@@ -9,10 +9,55 @@ def GetDB():
 
     return db
 
+
 def GetAllGuesses():
 
     # Connect, query all guesses and then return the data
     db = GetDB()
     guesses = db.execute("SELECT * FROM Guesses").fetchall()
     db.close()
+
     return guesses
+
+
+##################################
+### New code starts here
+##################################
+
+def CheckLogin(username, password):
+
+    db = GetDB()
+
+    # Ask the database for a single user matching the provided name
+    user = db.execute(
+        "SELECT * FROM Users WHERE username=?",
+        (username,)
+    ).fetchone()
+
+    db.close()
+
+    # Do they exist?
+    if user is not None:
+
+        # Is their password correct?
+        if check_password_hash(user['password'], password):
+
+            # Login successful
+            return user
+
+    # Login failed
+    return None
+
+def ResetPassword(username, new_password):
+    
+    db = GetDB()
+
+    hashed = generate_password_hash(new_password)
+
+    db.execute(
+        "UPDATE Users SET password=? WHERE username=?",
+        (hashed, username)
+    )
+
+    db.commit()
+    db.close()
--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 import db
 
 app = Flask(__name__)
 app.secret_key = "gtg"
 
+
 @app.route("/")
 def Home():
-    guessData = db.GetAllGuesses() # NOTE: the new line
+    guessData = db.GetAllGuesses()
     return render_template("index.html", guesses=guessData)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def Login():
+
+    error = None
+
+    if request.method == "POST":
+
+        username = request.form['username']
+        password = request.form['password']
+
+        user = db.CheckLogin(username, password)
+
+        if user:
+            session['id'] = user['id']
+            session['username'] = user['username']
+            return redirect("/")
+        else:
+            error = "Invalid username or password"
+
+    return render_template("login.html", error=error)
+
+
+@app.route("/logout")
+def Logout():
+    session.clear()
+    return redirect("/")
+
 
 app.run(debug=True, port=5000)
